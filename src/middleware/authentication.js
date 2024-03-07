b95084f665605f57d22cb2ff292fee660f18c62b
Patrick Motta <patrick.ramos.motta@gmial.com>
2024-03-07 10:42:37 -0300

    feat: adicionar middleware authentication

---
+++ src/middleware/authentication.js
@@ -0,0 +1,18 @@
+import jwtUtils from "../utils/jwtUtils";
+
+const verifyToken = ( req, res, next ) =>{
+   const token = req.headers['authorization'];
+
+   if(!token){
+      return res.status(401).json({ message: 'Token ausente' })
+   }
+
+   jwtUtils.verifyToken(token).then( decoded => {
+      req.user = decoded;
+      next();
+   }).catch(err => {
+      return res.status(403).json({ message: 'Token inválido' })
+   });
+}
+
+export default verifyToken;
